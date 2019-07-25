7571f6a3c90e8386fb1b5f23ed30473b863a454d
andys8 <andys8@users.noreply.github.com>
2019-07-25 21:39:12 +0200

    PBT blog post

--- open-browser.sh
+++ open-browser.sh
@@ -1,24 +1,23 @@
-#!/bin/bash
-
 # Vim in Elm
-xdg-open https://andys8.github.io/vim-emulation &
-xdg-open https://github.com/andys8/vim-emulation &
+https://andys8.github.io/vim-emulation
+https://github.com/andys8/vim-emulation
 
 # Property Based Testing
-xdg-open https://skillsmatter.com/skillscasts/6432 &
+https://skillsmatter.com/skillscasts/6432
+https://fsharpforfunandprofit.com/posts/property-based-testing-2/
 
 # Elm Compiler Test
-xdg-open https://github.com/andys8/elm-compiler-test &
-xdg-open https://travis-ci.org/andys8/elm-compiler-test &
+https://github.com/andys8/elm-compiler-test
+https://travis-ci.org/andys8/elm-compiler-test
 
 # Asset loader
-xdg-open https://github.com/halfzebra/create-elm-app/blob/master/template/README.md#adding-images-and-fonts &
-xdg-open https://github.com/andys8/poc-elm-asset-webpack-loader &
-xdg-open https://github.com/andys8/elm-asset-webpack-loader &
+https://github.com/halfzebra/create-elm-app/blob/master/template/README.md#adding-images-and-fonts
+https://github.com/andys8/poc-elm-asset-webpack-loader
+https://github.com/andys8/elm-asset-webpack-loader
 
 # Vim Tooling
-xdg-open https://github.com/elm-tooling/elm-vim &
+https://github.com/elm-tooling/elm-vim
 
 # Elm language server
-xdg-open https://microsoft.github.io/language-server-protocol &
-xdg-open https://github.com/elm-tooling/elm-language-server &
+https://microsoft.github.io/language-server-protocol
+https://github.com/elm-tooling/elm-language-server

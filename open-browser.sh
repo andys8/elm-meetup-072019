b9ea8e90195ce23c3ebddbd86e9fa21e9613c316
andys8 <andys8@users.noreply.github.com>
2019-07-25 18:51:13 +0200

    Links

---
+++ open-browser.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Vim in Elm
+xdg-open https://andys8.github.io/vim-emulation &
+xdg-open https://github.com/andys8/vim-emulation &
+
+# Property Based Testing
+xdg-open https://skillsmatter.com/skillscasts/6432 &
+
+# Elm Compiler Test
+xdg-open https://github.com/andys8/elm-compiler-test &
+xdg-open https://travis-ci.org/andys8/elm-compiler-test &
+
+# Asset loader
+xdg-open https://github.com/halfzebra/create-elm-app/blob/master/template/README.md#adding-images-and-fonts &
+xdg-open https://github.com/andys8/poc-elm-asset-webpack-loader &
+xdg-open https://github.com/andys8/elm-asset-webpack-loader &
+
+# Vim Tooling
+xdg-open https://github.com/elm-tooling/elm-vim &
+
+# Elm language server
+xdg-open https://microsoft.github.io/language-server-protocol &
+xdg-open https://github.com/elm-tooling/elm-language-server &
